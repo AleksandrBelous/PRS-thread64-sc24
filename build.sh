53ba26afdc9a972c7d10b654676132a4c56474d1
Aleksandr Belous <104652443+AleksandrBelous@users.noreply.github.com>
2025-07-11 09:57:04 +0000

    Handle SSE2 for m4ri only

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
-cd solvers/kissat-inc;
-./configure && make -j;
-cd ..;
-cd ..;
-make clean; make -j;
+ARCH="$(uname -m)"
+
+cd solvers/kissat-inc
+./configure && make -j
+cd ../..
+
+cd preprocess/m4ri-20140914
+if echo "$ARCH" | grep -qE "x86|amd64"; then
+  ./configure && make -j
+else
+  ./configure --disable-sse2 && make -j
+fi
+cd ../..
+
+make clean
+make -j
